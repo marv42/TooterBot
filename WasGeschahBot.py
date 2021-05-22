9264e136f4e5559421acfc5858f61b76a1a54ff6
Marvin <marv42+github@gmail.com>
2021-05-22 23:49:02 +0200

    initial commit

---
+++ WasGeschahBot.py
@@ -0,0 +1,45 @@
+from credentials import username, password, clientId, clientSecret
+from mastodon import Mastodon
+from datetime import date, timedelta
+
+
+def register():
+    Mastodon.create_app(
+        clientName,
+        api_base_url=apiBaseUrl,
+        to_file=clientCredentialsFile)
+
+
+def login():
+    mastodon = Mastodon(
+        client_id=clientId,  # TODO client_id=clientCredentialsFile,
+        client_secret=clientSecret,
+        api_base_url=apiBaseUrl)
+    version = mastodon.retrieve_mastodon_version()
+    mastodon.log_in(username, password, to_file=userCredentialsFile)
+
+
+def get_instance():
+    return Mastodon(
+        access_token=userCredentialsFile,
+        api_base_url=apiBaseUrl)
+
+
+def toot_daily():
+    mastodon = get_instance()
+    today = date.today()
+    # yesterday = today - timedelta(days=1)  # because of time zone differences
+    mastodon.toot(
+        f"Was geschah heute vor 100 Jahren?\nhttps://chroniknet.de/extra/was-war-am/?ereignisdatum={today.day}.{today.month}.{today.year}")
+
+
+clientName = 'pytooterappwasgeschah'
+apiBaseUrl = 'https://botsin.space'
+clientCredentialsFile = 'pytooter_clientcred_wasgeschah.secret'
+userCredentialsFile = 'pytooter_usercred_wasgeschah.secret'
+
+
+if __name__ == '__main__':
+    # register()  # this only needs to be done once
+    login()
+    toot_daily()
