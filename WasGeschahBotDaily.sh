a683a7206af69c0d80b1311bd32b5b9eb09e6a1e
Marvin <marv42+github@gmail.com>
2023-06-05 11:48:23 +0200

    change working dir

--- WasGeschahBotDaily.sh
+++ WasGeschahBotDaily.sh
@@ -3,5 +3,6 @@
 # TODO https://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+cd $DIR
 source $DIR/venv/bin/activate
 $DIR/venv/bin/python3 $DIR/WasGeschahBot.py
