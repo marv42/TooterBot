5c35153ec15503b292b9dd1e0184315a001f95f0
Marvin <marv42+github@gmail.com>
2024-02-10 00:03:07 +0100

    Constructor with api_base_url

--- WasGeschahBot.py
+++ WasGeschahBot.py
@@ -7,25 +7,26 @@ from credentials import username, password
 from mastodon import Mastodon, MastodonIllegalArgumentError
 from datetime import date
 
+API_BASE_URL = 'https://botsin.space'
 CLIENT_SECRET_FILE = 'pytooter_clientcred_wasgeschah.secret'
 
 
 def register():
     Mastodon.create_app(
         'pytooterapp_wasgeschah',
-        api_base_url='https://botsin.space',
+        api_base_url=API_BASE_URL,
         to_file=CLIENT_SECRET_FILE)
 
 
 def login():
-    mastodon = Mastodon(client_id=CLIENT_SECRET_FILE)
+    mastodon = Mastodon(client_id=CLIENT_SECRET_FILE, api_base_url=API_BASE_URL)
     version = mastodon.retrieve_mastodon_version()
     logging.debug(f"Mastodon version {version}")
     return mastodon.log_in(username, password)  # , to_file=userCredentialsFile)
 
 
 def get_instance(token):
-    return Mastodon(client_id=CLIENT_SECRET_FILE, access_token=token)
+    return Mastodon(client_id=CLIENT_SECRET_FILE, api_base_url=API_BASE_URL, access_token=token)
 
 
 def toot_daily(token):
@@ -36,9 +37,11 @@ https://chroniknet.de/extra/was-war-am/?ereignisdatum={today.day}.{today.month}.
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     # register(); sys.exit()  # this only needs to be done once
     try:
         access_token = login()
+        logging.debug(f"access_token: {access_token}")
         toot_daily(access_token)
         get_instance(access_token).revoke_access_token()
     except MastodonIllegalArgumentError as e:
