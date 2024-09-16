6ead608409e379f1fbdc0c507889183afa109b55
Marvin <marv42+github@gmail.com>
2024-09-16 20:18:37 +0200

    Rearrange

--- CalvinAndHobbesBot.py
+++ CalvinAndHobbesBot.py
@@ -20,10 +20,9 @@ def parse_args():
 calvin_and_hobbes_bot = TooterBot(CLIENT_SECRET_FILE)
 # calvin_and_hobbes_bot.register(CLIENT_NAME); sys.exit()  # this only needs to be done once
 
-url = urllib.request.urlopen('https://www.gocomics.com/random/calvinandhobbes').geturl()
-text = f"random #CalvinAndHobbes comic:\n{url}"
-if not parse_args().random:
-    today = date.today()
-    # yesterday = today - timedelta(days=1)  # because of time zone differences
-    text = f"daily #CalvinAndHobbes comic:\nhttps://www.gocomics.com/calvinandhobbes/{today.year}/{today.month:02d}/{today.day:02d}"
+today = date.today()
+text = f"daily #CalvinAndHobbes comic:\nhttps://www.gocomics.com/calvinandhobbes/{today.year}/{today.month:02d}/{today.day:02d}"
+if parse_args().random:
+    url = urllib.request.urlopen('https://www.gocomics.com/random/calvinandhobbes').geturl()
+    text = f"random #CalvinAndHobbes comic:\n{url}"
 calvin_and_hobbes_bot.login_and_toot(username, password, text)
