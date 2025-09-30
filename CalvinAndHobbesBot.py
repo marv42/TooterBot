f0612da414cd5b2c551847eff84c1c7750571e62
Marvin <marv42+github@gmail.com>
2025-09-30 22:28:14 +0200

    Calculate random C&H comic date
    because the randum URL does not work any more

--- CalvinAndHobbesBot.py
+++ CalvinAndHobbesBot.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import argparse
-import urllib.request
-from datetime import date
+from datetime import date, timedelta
+from random import randrange
 
 from calvin_and_hobbes_credentials import access_token
 from TooterBot import TooterBot
 
 CLIENT_NAME = 'pytooterappcalvinandhobbes'
 CLIENT_SECRET_FILE = 'pytooter_clientcred_calvinandhobbes.secret'
+DATE_OF_THE_FIRST_COMIC = '1985-11-18'
 
 
 def parse_args():
@@ -19,9 +20,16 @@ def parse_args():
 calvin_and_hobbes_bot = TooterBot(CLIENT_SECRET_FILE)
 # calvin_and_hobbes_bot.register(CLIENT_NAME); sys.exit()  # this only needs to be done once
 
-today = date.today()
-text = f"daily #CalvinAndHobbes comic:\nhttps://www.gocomics.com/calvinandhobbes/{today.year}/{today.month:02d}/{today.day:02d}"
+comic_date = date.today()
+text = "daily"
+
 if parse_args().random:
-    url = urllib.request.urlopen('https://www.gocomics.com/random/calvinandhobbes').geturl()
-    text = f"random #CalvinAndHobbes comic:\n{url}"
+    first_date = date.fromisoformat(DATE_OF_THE_FIRST_COMIC)
+    date_delta = comic_date - first_date
+    random_number_of_days = randrange(date_delta.days)
+    comic_date = first_date + timedelta(days=random_number_of_days)
+    text = "random"
+
+text = f"{text} #CalvinAndHobbes comic:\n" \
+       f"https://www.gocomics.com/calvinandhobbes/{comic_date.year}/{comic_date.month:02d}/{comic_date.day:02d}"
 calvin_and_hobbes_bot.toot(access_token, text)
