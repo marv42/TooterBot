51f9019a92150c38b952a5bab416b2f549869771
Marvin <marv42+github@gmail.com>
2025-10-01 22:42:28 +0200

    Take comic strip url for random comic

--- CalvinAndHobbesBot.py
+++ CalvinAndHobbesBot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import argparse
+import re
+import requests
 from datetime import date, timedelta
 from random import randrange
 
@@ -8,6 +10,7 @@ from TooterBot import TooterBot
 
 CLIENT_NAME = 'pytooterappcalvinandhobbes'
 CLIENT_SECRET_FILE = 'pytooter_clientcred_calvinandhobbes.secret'
+CALVIN_AND_HOBBES_BASE_URL = 'https://www.gocomics.com/calvinandhobbes/'
 DATE_OF_THE_FIRST_COMIC = '1985-11-18'
 
 
@@ -16,11 +19,14 @@ def parse_args():
     parser.add_argument('--random', action='store_true', help='toot randomly')
     return parser.parse_args()
 
+def get_comic_site_url(date):
+    return f"{CALVIN_AND_HOBBES_BASE_URL}{date.year}/{date.month:02d}/{date.day:02d}"
 
 calvin_and_hobbes_bot = TooterBot(CLIENT_SECRET_FILE)
 # calvin_and_hobbes_bot.register(CLIENT_NAME); sys.exit()  # this only needs to be done once
 
 comic_date = date.today()
+url = get_comic_site_url(comic_date)
 text = "daily"
 
 if parse_args().random:
@@ -28,8 +34,13 @@ if parse_args().random:
     date_delta = comic_date - first_date
     random_number_of_days = randrange(date_delta.days)
     comic_date = first_date + timedelta(days=random_number_of_days)
+    url = get_comic_site_url(comic_date)
+    result = requests.get(url)
+    if not result.ok:
+        raise Exception("Error getting web site")
+    m = re.search('(https://featureassets.gocomics.com/assets/.*?)"', result.text)
+    url = m.group(1)
     text = "random"
 
-text = f"{text} #CalvinAndHobbes comic:\n" \
-       f"https://www.gocomics.com/calvinandhobbes/{comic_date.year}/{comic_date.month:02d}/{comic_date.day:02d}"
+text = f"{text} #CalvinAndHobbes comic:\n{url}"
 calvin_and_hobbes_bot.toot(access_token, text)
